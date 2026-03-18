16edcc169df0720fe7b4332abf886d80f046e8c6
sik2 <bickwang123@gmail.com>
2026-03-18 14:41:13 +0900

    0004

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,24 @@ import { useState } from 'react'
 function App() {
     const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
 
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+
+        if (form.todo.value.length == 0) {
+            alert('할일을 입력해주세요.')
+            return
+        }
+
+        setTodos([...todos, form.todo.value])
+    }
+
     return (
         <>
+            <form onSubmit={handleOnSubmit}>
+                <input type="text" name="todo" />
+                <button>등록</button>
+            </form>
             <ul>
                 {todos.map((item, i) => (
                     <li key={i}>{item}</li>
