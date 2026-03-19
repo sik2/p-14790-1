a0688fb4d95c462c5bbbcd0a732bb33bde52b6f8
sik2 <bickwang123@gmail.com>
2026-03-19 15:19:13 +0900

    0018

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 function TodoItem({ item, deleteTodo, toggleTodo }) {
     return (
         <li key={item.id} style={{ textDecoration: item.completed ? 'line-through' : 'none' }}>
             <input type="checkbox" onChange={() => toggleTodo(item.id)} checked={item.completed} />
             <span>
-                {item.id} / {item.todo}
+                <Link to={`/todos/${item.id}`}>
+                    {item.id} / {item.todo}
+                </Link>
             </span>
             <button onClick={() => deleteTodo(item.id)}>X</button>
         </li>
--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 function TodoDetail() {
-    return <>todo</>
+    const { id } = useParams()
+    const [todo, setTodo] = useState({})
+
+    useEffect(() => {
+        fetch(`https://dummyjson.com/todos/${id}`)
+            .then((res) => res.json())
+            .then((res) => setTodo(res))
+    }, [])
+
+    return (
+        <>
+            <div>{todo.id} 번 할일</div>
+            <h1>할일: {todo.todo}</h1>
+            <div>할일여부: {todo.completed ? '✅' : '❌'}</div>
+        </>
+    )
 }
 
 export default TodoDetail
